dcae22d5b2259bce8b73017eb5fca4046db001c2
Wei-Chung Chen <wcchen101@gmail.com>
2017-10-21 21:27:56 -0700

    add: starter code

---
+++ src/Components/App.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+//css
+require('style-loader!./App.css');
+
+class App extends React.Component {
+
+  render() {
+    return (
+    <div className='app'>
+      <h1>Movie Reviews</h1>
+
+    </div>
+    );
+  }
+}
+
+export default App;
